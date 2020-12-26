17442deb7b0d204970ba702a14e6bb0517e66dfc
Jonathan Haines <jonno.haines@gmail.com>
2020-12-27 10:14:57 +1100

    use @newfrontdoor/collapse

--- components/navigation.js
+++ components/navigation.js
@@ -1,12 +1,20 @@
 /** @jsx jsx */
-import {useState} from 'react';
+import {useRef} from 'react';
 import {jsx, Box, Flex, Button, Link as UILink, ThemeProvider} from 'theme-ui';
 import Link from 'next/link';
+import {useResponsiveValue} from '@theme-ui/match-media';
+import {useCollapse} from '@newfrontdoor/collapse';
 import Logo from '../public/logo2.svg';
 import Menu from './menu';
-import * as Collapse from './collapse';
 
 const Navigation = ({menuitems}) => {
+  const contentRef = useRef(null);
+  const isDisabled = useResponsiveValue([false, true]);
+  const {getToggleProps, getCollapseProps} = useCollapse({
+    contentRef,
+    isDisabled
+  });
+
   return (
     <ThemeProvider
       theme={{
@@ -17,53 +25,53 @@ const Navigation = ({menuitems}) => {
         }
       }}
     >
-      <Collapse.Manager>
-        <Flex
-          as="header"
-          sx={{
-            bg: 'primary',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            justifyContent: 'space-around',
-            flexWrap: 'wrap',
-            alignItems: 'flex-end'
-          }}
+      <Flex
+        as="header"
+        sx={{
+          bg: 'primary',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          justifyContent: 'space-around',
+          flexWrap: 'wrap',
+          alignItems: 'flex-end'
+        }}
+      >
+        <Box pb={3} sx={{flex: '0 1 auto', height: '110px'}}>
+          <Link passHref href="/" as="/">
+            <UILink sx={{color: 'background'}}>
+              <Logo sx={{height: '80px'}} />
+            </UILink>
+          </Link>
+        </Box>
+        <Box pb={3} sx={{flex: '1 0 100%', display: ['block', ' none']}}>
+          <Button
+            {...getToggleProps()}
+            sx={{
+              display: 'block',
+              margin: '0 auto',
+              border: '1px solid #c2b49a',
+              background: '#c2b49a',
+              color: 'text',
+              borderRadius: '0',
+              width: '100%',
+              maxWidth: '250px',
+              padding: '12px',
+              textTransform: 'uppercase'
+            }}
+          >
+            Menu
+          </Button>
+        </Box>
+        <Box
+          px={4}
+          sx={{flex: ['1 0 100%', '0 1 auto']}}
+          {...getCollapseProps()}
         >
-          <Box pb={3} sx={{flex: '0 1 auto', height: '110px'}}>
-            <Link passHref href="/" as="/">
-              <UILink sx={{color: 'background'}}>
-                <Logo sx={{height: '80px'}} />
-              </UILink>
-            </Link>
+          <Box ref={contentRef}>
+            <Menu items={menuitems} />
           </Box>
-          <Box pb={3} sx={{flex: '1 0 100%', display: ['block', ' none']}}>
-            <Collapse.Toggle>
-              <Button
-                type="button"
-                sx={{
-                  display: 'block',
-                  margin: '0 auto',
-                  border: '1px solid #c2b49a',
-                  background: '#c2b49a',
-                  color: 'text',
-                  borderRadius: '0',
-                  width: '100%',
-                  maxWidth: '250px',
-                  padding: '12px',
-                  textTransform: 'uppercase'
-                }}
-              >
-                Menu
-              </Button>
-            </Collapse.Toggle>
-          </Box>
-          <Box px={4} sx={{flex: ['1 0 100%', '0 1 auto']}}>
-            <Collapse.Panel expanded={[false, true]}>
-              <Menu items={menuitems} />
-            </Collapse.Panel>
-          </Box>
-        </Flex>
-      </Collapse.Manager>
+        </Box>
+      </Flex>
     </ThemeProvider>
   );
 };
